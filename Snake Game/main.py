05f1db584af71c7c2fb7d321899d564e315cd189
Mohammad Amil Bhat <61305746+amilbhat@users.noreply.github.com>
2023-01-30 09:00:52 +0530

    Added highscore in snake game

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -40,14 +40,14 @@ while game_is_on:
 
     # Detect Collision wil wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 
