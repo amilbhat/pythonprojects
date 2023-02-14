ba1ccac2e09dd5ebf1ab056bd8389440c9d9adf0
Mohammad Amil Bhat <61305746+amilbhat@users.noreply.github.com>
2023-02-14 21:27:01 +0530

    Added Error Handling for non alphabetic inputs

--- NATO phonetic alphabet/main.py
+++ NATO phonetic alphabet/main.py
@@ -3,6 +3,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+continue_loop = True
+while continue_loop:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry Only Letters in the alphabet please")
+    else:
+        print(output_list)
+        continue_loop = False
+
