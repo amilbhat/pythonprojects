035f941e5144cc0b5b58d49f11d4ff0ccfc97197
Mohammad Amil Bhat <61305746+amilbhat@users.noreply.github.com>
2023-02-17 11:32:16 +0530

    Added Automatic Email Sender (Python smtplib))

---
+++ Automatic Email Sender (smtplib)/Monday Motivation/main.py
@@ -0,0 +1,24 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs="RECIEVER EMAIL",
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+
